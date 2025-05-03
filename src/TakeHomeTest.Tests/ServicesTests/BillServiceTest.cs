444b1be513f6387feb4581d7ed44b9676aa4be6b
schonssj <jvschons@gmail.com>
2025-05-03 16:05:29 -0300

    test: add tests for service layer

---
+++ src/TakeHomeTest.Tests/ServicesTests/BillServiceTest.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Configuration;
+using TakeHomeTest.API.Repositories;
+using TakeHomeTest.API.Services;
+
+namespace TakeHomeTest.Tests.ServicesTests
+{
+    [TestClass]
+    public class BillServiceTest
+    {
+        private BillService _billService;
+        private BillRepository _billRepository;
+        private LegislatorRepository _legislatorRepository;
+        private VoteRepository _voteRepository;
+        private VoteResultRepository _voteResultRepository;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            var appSettingsValues = new Dictionary<string, string>
+            {
+                {"Settings:PathToBillsCSV", "C:\\fullstack-challenge\\src\\TakeHomeTest\\Data\\bills.csv"},
+                {"Settings:PathToLegislatorsCSV", "C:\\fullstack-challenge\\src\\TakeHomeTest\\Data\\legislators.csv"},
+                {"Settings:PathToVotesCSV", "C:\\fullstack-challenge\\src\\TakeHomeTest\\Data\\votes.csv"},
+                {"Settings:PathToVoteResultsCSV", "C:\\fullstack-challenge\\src\\TakeHomeTest\\Data\\vote_results.csv"},
+            };
+
+            var configuration = new ConfigurationBuilder().AddInMemoryCollection(appSettingsValues).Build();
+
+            _billRepository = new BillRepository(configuration);
+            _legislatorRepository = new LegislatorRepository(configuration);
+            _voteRepository = new VoteRepository(configuration);
+            _voteResultRepository = new VoteResultRepository(configuration);
+            _billService = new BillService(_billRepository, _legislatorRepository, _voteRepository, _voteResultRepository);
+        }
+
+        [TestMethod]
+        public void GetBillsResults_ReturnsData()
+        {
+            var result = _billService.GetBillsResults();
+            Assert.IsNotNull(result);
+        }
+    }
+}
---
+++ src/TakeHomeTest.Tests/ServicesTests/LegislatorServiceTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Configuration;
+using TakeHomeTest.API.Repositories;
+using TakeHomeTest.API.Services;
+
+namespace TakeHomeTest.Tests.ServicesTests
+{
+    [TestClass]
+    public class LegislatorServiceTests
+    {
+        private LegislatorService _legislatorService;
+        private LegislatorRepository _legislatorRepository;
+        private VoteResultRepository _voteResultRepository;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            var appSettingsValues = new Dictionary<string, string>
+            {
+                {"Settings:PathToBillsCSV", "C:\\fullstack-challenge\\TakeHomeTest\\Data\\bills.csv"},
+                {"Settings:PathToLegislatorsCSV", "C:\\fullstack-challenge\\TakeHomeTest\\Data\\legislators.csv"},
+                {"Settings:PathToVotesCSV", "C:\\fullstack-challenge\\TakeHomeTest\\Data\\votes.csv"},
+                {"Settings:PathToVoteResultsCSV", "C:\\fullstack-challenge\\TakeHomeTest\\Data\\vote_results.csv"},
+            };
+
+            var configuration = new ConfigurationBuilder().AddInMemoryCollection(appSettingsValues).Build();
+
+            _legislatorRepository = new LegislatorRepository(configuration);
+            _voteResultRepository = new VoteResultRepository(configuration);
+            _legislatorService = new LegislatorService(_legislatorRepository, _voteResultRepository);
+        }
+
+        [TestMethod]
+        public void GetLegislatorsVotes_ReturnsData()
+        {
+            var result = _legislatorService.GetLegislatorsVotes();
+            Assert.IsNotNull(result);
+        }
+    }
+}
