9366678349f7ac3eb27180e65d88211a5f4ca5eb
schonssj <jvschons@gmail.com>
2025-05-03 16:01:04 -0300

    feat: add controllers

---
+++ src/TakeHomeTest.API/Controllers/BillController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TakeHomeTest.API.Services;
+
+namespace TakeHomeTest.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BillController : Controller
+    {
+        private readonly BillService _billService;
+
+        public BillController(BillService billService)
+        {
+            _billService = billService;
+        }
+
+        /// <summary>
+        /// Returns all bills and their votes.
+        /// </summary>
+        [HttpGet("GetBillsResults")]
+        public IActionResult GetBillsResults()
+        {
+            return Ok(_billService.GetBillsResults().OrderBy(b => b.Id));
+        }
+    }
+}
---
+++ src/TakeHomeTest.API/Controllers/LegislatorController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TakeHomeTest.API.Services;
+
+namespace TakeHomeTest.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LegislatorController : Controller
+    {
+        private readonly LegislatorService _legislatorService;
+        
+        public LegislatorController(LegislatorService legislatorService)
+        {
+            _legislatorService = legislatorService;
+        }
+
+        /// <summary>
+        /// Returns all legislators and their votes.
+        /// </summary>
+        [HttpGet("GetLegislatorsVotes")]
+        public IActionResult GetLegislatorsVotes()
+        {
+            return Ok(_legislatorService.GetLegislatorsVotes().OrderBy(l => l.LegislatorId));
+        }
+    }
+}
