b19109ebc2822a6014677c9ceae2fd0b364788f1
schonssj <jvschons@gmail.com>
2025-05-03 15:59:44 -0300

    feat: add services

---
+++ src/TakeHomeTest.API/Services/BillService.cs
@@ -0,0 +1,42 @@
+﻿using TakeHomeTest.API.DTO;
+using TakeHomeTest.API.Models;
+using TakeHomeTest.API.Repositories;
+
+namespace TakeHomeTest.API.Services
+{
+    public class BillService
+    {
+        private readonly BillRepository _billRepository;
+        private readonly LegislatorRepository _legislatorRepository;
+        private readonly VoteRepository _voteRepository;
+        private readonly VoteResultRepository _voteResultRepository;
+
+        public BillService(BillRepository billRepository, LegislatorRepository legislatorRepository, VoteRepository voteRepository, VoteResultRepository voteResultRepository)
+        {
+            _billRepository = billRepository;
+            _legislatorRepository = legislatorRepository;
+            _voteRepository = voteRepository;
+            _voteResultRepository = voteResultRepository;
+        }
+
+        public IEnumerable<BillResultsDTO> GetBillsResults()
+        {
+            var bills = _billRepository.GetAll();
+            foreach (var bill in bills)
+            {
+                var primarySponsor = _legislatorRepository.GetById(bill.PrimarySponsor);
+                var vote = _voteRepository.GetByBillId(bill.Id);
+                var voteResults = _voteResultRepository.GetByVoteId(vote.Id);
+
+                yield return new BillResultsDTO
+                (
+                    bill.Id,
+                    bill.Title,
+                    voteResults.Where(vr => vr.VoteType.Equals(VoteType.YEA)).Count(),
+                    voteResults.Where(vr => vr.VoteType.Equals(VoteType.NAY)).Count(),
+                    primarySponsor?.Name ?? ""
+                );
+            }
+        }
+    }
+}
---
+++ src/TakeHomeTest.API/Services/LegislatorService.cs
@@ -0,0 +1,35 @@
+﻿using TakeHomeTest.API.DTO;
+using TakeHomeTest.API.Models;
+using TakeHomeTest.API.Repositories;
+
+namespace TakeHomeTest.API.Services
+{
+    public class LegislatorService
+    {
+        private readonly LegislatorRepository _legislatorRepository;
+        private readonly VoteResultRepository _voteResultRepository;
+
+        public LegislatorService(LegislatorRepository legislatorRepository, VoteResultRepository voteResultRepository)
+        {
+            _legislatorRepository = legislatorRepository;
+            _voteResultRepository = voteResultRepository;
+        }
+
+        public IEnumerable<LegislatorVotesDTO> GetLegislatorsVotes()
+        {
+            var legislators = _legislatorRepository.GetAll();
+            foreach (var legislator in legislators)
+            {
+                var voteResults = _voteResultRepository.GetByLegislatorId(legislator.Id);
+
+                yield return new LegislatorVotesDTO
+                (
+                    legislator.Id,
+                    legislator.Name,
+                    voteResults.Where(vr => vr.VoteType.Equals(VoteType.YEA)).Count(),
+                    voteResults.Where(vr => vr.VoteType.Equals(VoteType.NAY)).Count()
+                );
+            }
+        }
+    }
+}
