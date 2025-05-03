1dbffa537a2630267e50c0f4833afe487556f5ba
schonssj <jvschons@gmail.com>
2025-05-03 16:00:26 -0300

    feat: add data transfer objects

---
+++ src/TakeHomeTest.API/DTO/BillResultsDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace TakeHomeTest.API.DTO
+{
+    public record BillResultsDTO
+    (
+        int Id, 
+        string BillTitle, 
+        int Supporters, 
+        int Opposers, 
+        string PrimarySponsor
+    );
+}
---
+++ src/TakeHomeTest.API/DTO/LegislatorVotesDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace TakeHomeTest.API.DTO
+{
+    public record LegislatorVotesDTO
+    (
+        int LegislatorId,
+        string LegislatorName,
+        int SupportedBills,
+        int OpposedBills
+    );
+}
